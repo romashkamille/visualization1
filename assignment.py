091f73589cf3711fd950dbd8d7988b7c96ba2340
tsuhsuanchu <78448819+tsuhsuanchu@users.noreply.github.com>
2021-09-24 16:10:15 -0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -141,8 +141,9 @@ app.layout = html.Div(children=[
                 Output(component_id='plot2', component_property='children'),
                 Output(component_id='plot3', component_property='children'),
                 Output(component_id='plot4', component_property='children'),
-                Output(component_id='plot5', component_property='children')
-               ],
+                Output(component_id='plot5', component_property='children')],
+               [Input(component_id='input-type', component_property='value'),
+                Input(component_id='input-year', component_property='value')],
                # REVIEW4: Holding output state till user enters all the form information. In this case, it will be chart type and year
                [State("plot1", 'children'), State("plot2", "children"),
                 State("plot3", "children"), State("plot4", "children"),
